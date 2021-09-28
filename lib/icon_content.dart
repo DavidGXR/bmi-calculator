7e2e2f0d5a27ced0a34a2ffa2c5cdb7390dea9dc
Im David <imdavid1007@gmail.com>
2021-09-28 19:11:29 +0700

    Half way commit

---
+++ lib/icon_content.dart
@@ -0,0 +1,33 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/material.dart';
+
+
+
+class IconContent extends StatelessWidget {
+
+  final IconData icon;
+  final String title;
+
+  IconContent({this.icon, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+
+        Text(
+          title,
+          style: kLabelTextStyle
+        )
+      ],
+    );
+  }
+}
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
+import 'constants.dart';
+
+enum Gender {
+  male,
+  female
+}
 
 class InputPage extends StatefulWidget {
 
@@ -7,69 +16,116 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  Gender selectedGender;
+  int height = 180;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
+        centerTitle: true,
       ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    onPress: () {
+                      selectedGender = Gender.male;
+                      setState(() {
+                      });
+                    },
+                    color: selectedGender == Gender.male ? kActiveCardColour : kInactiveColour,
+                    cardChild: IconContent(icon: FontAwesomeIcons.mars, title: 'MALE'),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    onPress: () {
+                      selectedGender = Gender.female;
+                      setState(() {
+                      });
+                    },
+                    color: selectedGender == Gender.female ? kActiveCardColour : kInactiveColour,
+                    cardChild: IconContent(icon: FontAwesomeIcons.venus, title: 'FEMALE'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Expanded(
+            child: ReusableCard(
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
-                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  Text(
+                    'HEIGHT',
+                    style: kLabelTextStyle,
                   ),
-                  Expanded(
-                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        height.toString(),
+                        style: kNumberTextStyle,
+                      ),
+                      Text(
+                        'cm',
+                        style: kLabelTextStyle,
+                      )
+                    ],
                   ),
+                  Slider(
+                    value: height.toDouble(),
+                    min: 120.0,
+                    max: 220.0,
+                    activeColor: kBottomContainerColor,
+                    inactiveColor: Color(0xFF8D8E98),
+                    onChanged: (double newValue) {
+                      setState(() {
+                        height = newValue.round();
+                      });
+                    },
+                  )
                 ],
               ),
+              color: kActiveCardColour
             ),
+          ),
 
-            Expanded(
-              child: ReusableCard(color: Color(0xFF1D1E33)),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: ReusableCard(color: kActiveCardColour),
+                ),
+                Expanded(
+                  child: ReusableCard(color: kActiveCardColour),
+                ),
+              ],
             ),
+          ),
 
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: ReusableCard(color: Color(0xFF1D1E33)),
-                  ),
-                  Expanded(
-                    child: ReusableCard(color: Color(0xFF1D1E33)),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          Container(
+            height: kBottomContainerHeight,
+            width: double.infinity,
+            margin: EdgeInsets.only(top: 10.0),
+            color: kBottomContainerColor,
+          )
+        ],
       ),
     );
   }
 }
 
-class ReusableCard extends StatelessWidget {
-
-  final Color color;
-
-  ReusableCard({@required this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(10)
-      ),
-    );
-  }
-}
 
